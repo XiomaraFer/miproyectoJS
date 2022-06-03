cda1ecd490a5fd56c1b8764181d8fa22ec0b7048
XiomaraFer <102708588+XiomaraFer@users.noreply.github.com>
2022-06-03 18:41:04 -0500

    tarea1

--- js/tarea.js
+++ js/tarea.js
@@ -1,24 +1,24 @@
-let datosfijo=0.20;
-let datosclaro=0.40;
-let datosmovistar=0.60;
-let datosentel=1.00;
+let datofijo=0.20;
+let datoclaro=0.40;
+let datomovistar=0.60;
+let datoentel=1.00;
 
-let cualoperador=prompt(`Escribe¿A que operador llamaste?\na.Fijo\nb.Claro\nc.Movistar\nd.Entel`);
+let cualoperador=prompt(`Escribe¿A que operador llamaste?\n*fijo\n*claro\n*movistar\n*entel`);
 let minutos=parseInt(prompt(`¿Cuantos minutos duro la llamada?`));
 
-let presiofijo=parseFloat(minutos*datosfijo);
-let presioclaro=parseFloat(minutos*datosclaro);
-let presiomovistar=parseFloat(minutos*datosmovistar);
-let presioentel=parseFloat(minutos*datosentel);
+let costofijo=parseFloat(minutos*datofijo);
+let costoclaro=parseFloat(minutos*datoclaro);
+let costomovistar=parseFloat(minutos*datomovistar);
+let costoentel=parseFloat(minutos*datoentel);
 
 if(cualoperador=="fijo"){
-    document.write(`El costo de la llamada por ${minutos} minutos a Fijo es S/ ${presiofijo}`);
+    document.write(`El costo de la llamada por ${minutos} minutos a Fijo es S/ ${costofijo}`);
 }else if(cualoperador=="claro"){
-    document.write(`El costo de la llamada por ${minutos} minutos en Claro es S/ ${presioclaro}`);
+    document.write(`El costo de la llamada por ${minutos} minutos en Claro es S/ ${costoclaro}`);
 }else if(cualoperador=="movistar"){
-   document.write(`El costo de la llamada por ${minutos} minutos en Movistar es S/ ${presiomovistar}`);
+   document.write(`El costo de la llamada por ${minutos} minutos en Movistar es S/ ${costomovistar}`);
 }else if(cualoperador=="entel"){
-    document.write(`El costo de la llamada por ${minutos}minutos en Entel es S/ ${presioentel}`);
+    document.write(`El costo de la llamada por ${minutos}minutos en Entel es S/ ${costoentel}`);
 }else {
     alert("No se identifico la operadora")
 }
